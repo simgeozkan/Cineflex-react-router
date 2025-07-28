899d6226c647b31e5bfd10981f2d13809316e17f
simge <k.simgeozkan@gmail.com>
2025-07-28 20:54:38 +0200

    welcome.tsx

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -16,7 +16,7 @@ function Welcome() {
   useEffect(() => {
     console.log("userEmail state güncellendi:", userEmail);
   }, []);
-
+ set
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
